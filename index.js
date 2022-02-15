f95de73d5debfe260a5e7163c7bd20ee26a156d3
fbergui <f.bergui.1268@vallauri.edu>
2022-02-15 09:20:01 +0100

    Base style for the homepage

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 
-
-
-
-
-
-
-
+const apikey="&apikey=N8Z6KN85A1KXUKDG";
+const apidemo="&apikey=demo";
+const localHost ="http://localhost:3000";
+let nomeIndice=["IBM","Sony","Alibaba","Microsoft","Xiaomi"];
+let indici=["IBM","MSFT","SNE","BABA","XIACF"];
 
 
 
 $(document).ready(function () {
-    getGlobalQuotes("IBM");
+
+    caricaCmb();
+
+
+    function caricaCmb() {
+        let i=0;
+        for (const indice of indici) {
+            
+            $("<option>").val(indice).html(nomeIndice[i]).appendTo($("#cmbAziende"))
+            i++;
+
+        }
+    }
+    
+    getGlobalQuotes($("#cmbAziende").val());
+
 });
 
 
 function getGlobalQuotes(symbol) {
-    let url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + symbol + "&apikey=N8Z6KN85A1KXUKDG";
-    $.getJSON(url,
-        function (data) {
+    let url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + symbol + apidemo;
+    $.getJSON(url,function (data) {
             $("#symbol").text(data["Global Quote"]["01. symbol"]);
             let globalQuoteData = data["Global Quote"];
             $("#previousClose").text(globalQuoteData["08. previous close"]);
