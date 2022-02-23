98154d657a1a46cba4f9682a144b7418b4dbc372
fbergui <f.bergui.1268@vallauri.edu>
2022-02-23 22:55:53 +0100

    Search working, need to fix some graphics

--- index.js
+++ index.js
@@ -4,13 +4,18 @@ const localHost ="http://localhost:3000";
 let nomeIndice=["IBM","Sony","Alibaba","Microsoft","Xiaomi"];
 let indici=["IBM","SNE","BABA","MSFT","XIACF"];
 let jsonDati=[];
+let jsonRicerche=[];
 let j=0;
-
+let stringSearch;
+let n=0;
 
 $(document).ready(function () {
 
+    let _divCerca = $("#divCerca");
+
     caricaCmb();
     getGlobalQuotes($("#cmbAziende").val());
+    $("#btnCerca").addClass("disabled")
 
     function caricaCmb() {
         let i=0;
@@ -26,7 +31,6 @@ $(document).ready(function () {
     
     function datiTabella(data) {
         
-        console.log(jsonDati)
         $("#symbol").text(data["Global Quote"]["01. symbol"]);
         let globalQuoteData = data["Global Quote"];
         $("#previousClose").text(globalQuoteData["08. previous close"]+' $');
@@ -63,6 +67,51 @@ $(document).ready(function () {
             });
         }
         
+    }
+
+    $("#txtRicerca").keydown(function (e) {
+        if(e.keyCode  === 32) 
+        {
+            e.preventDefault();
+            return false;
+        }
+    })
+
+    $("#txtRicerca").on("keyup",function () {
+        stringSearch= $(this).val()
+        _divCerca.html("")
+        if((`${stringSearch.length}`)>=2)
+        {
+            url="https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords="+stringSearch+"&apikey=N8Z6KN85A1KXUKDG";
+            $.getJSON(url, function (data) {
+                generaSuggerimenti(data.bestMatches)
+            }).fail(errore)
+        }
+    }).on("click",function () {
+        generaSuggerimenti(jsonRicerche)
+    })
+
+    $("<body>").off("click","#txtRicerca",function () {
+        _divCerca.html("");
+    })
+    
+    function generaSuggerimenti(data) {
+        n=0;
+        for (const suggestion of data) {
+            jsonRicerche[n]=suggestion;
+            n++;
+            let _div=  $("<div>").addClass("d-flex flex-row").appendTo(_divCerca).attr("id","ricerca")
+                   .on("click",function () {
+                       getGlobalQuotes(suggestion["1. symbol"]);
+                       _divCerca.html("");
+                   })
+                   $("<div>").addClass("d-flex justify-content-start").html(suggestion["1. symbol"]).appendTo(_div)
+                   $("<div>").addClass("d-flex justify-content-end").html(suggestion["2. name"]).appendTo(_div)
+                   /** 
+                   $("#divCerca>div:nth-of-type("+n+")").mouseenter(function () {
+                       $(this).css("background-color","grey")
+                   })*/
+        }
     }
 
 });
